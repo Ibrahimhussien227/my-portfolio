7ebc53902d4be6d63648ab17d15cf7fb6401af27
Ibrahim Hussein <65757724+Ibrahimhussien227@users.noreply.github.com>
2025-11-12 20:10:21 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <main>
+      <div>
+        <h1 className="text-3xl underline">
+          Welcom to my Portfolio!
+        </h1>
+      </div>
+    </main>
+  );
+};
+
+export default App;
